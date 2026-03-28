6a76e905bf08cfdc75c1146d83f90719431060a0
urickjf <34625156+urickjf@users.noreply.github.com>
2026-03-27 21:30:20 -0300

    Update puppeteer.config.cjs

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -1,10 +1,1 @@
-const { join } = require('path');
 
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // Diz ao Puppeteer para salvar o Chrome dentro da pasta do projeto
-  // Isso evita que o Render "esqueça" o navegador após o build
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
